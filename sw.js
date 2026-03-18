2376642fca74afb23f84cdcc92397446fd69f411
sg010404-blip <sg010404@gmail.com>
2026-03-19 00:21:46 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,18 @@
-const CACHE_NAME = "focus-timer-v1";
+const CACHE="pomodoro-v1";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(c=>c.addAll([
+"./",
+"./index.html",
+"./manifest.json",
+"./icon.png"
+]))
+);
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r||fetch(e.request))
+);
 });
