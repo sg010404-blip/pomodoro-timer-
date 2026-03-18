3818f9e50c09d1b1490f07c741105fd465c27fc4
sg010404-blip <sg010404@gmail.com>
2026-03-19 00:37:54 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
-const CACHE="pomodoro-v1";
+const CACHE_NAME = "pomodoro-v1";
 
-self.addEventListener("install",e=>{
+self.addEventListener("install", e=>{
 e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll([
+caches.open(CACHE_NAME).then(cache=>{
+return cache.addAll([
 "./",
 "./index.html",
 "./manifest.json",
 "./icon.png"
-]))
+]);
+})
 );
+self.skipWaiting();
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("activate", e=>{
+e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", e=>{
 e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
+caches.match(e.request).then(res=>{
+return res || fetch(e.request);
+})
 );
 });
